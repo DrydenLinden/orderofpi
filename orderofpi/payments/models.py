ec2d1dbdd7a3d2891a64f1c4bc0b4dbc21e3a2d3
cthib <cortland.thibodeau@gmail.com>
2017-07-10 14:14:11 -0700

    Oops, removed .pyc files and added migrations that were supposed to be there in the last PR...

--- orderofpi/payments/models.py
+++ orderofpi/payments/models.py
@@ -1,18 +1,17 @@
 from django.db import models
-from ..contracts.models import Contract
 
 
 # Transaction model
 class Transaction(models.Model):
-    contract = models.ForeignKey(Contract, on_delete=models.PROTECT)
+    contract = models.ForeignKey('contracts.Contract', on_delete=models.PROTECT)
 
     # Transaction basic details
     TYPE_CHOICES = (
         ('online_cc', 'Online Credit Card'),
-        ('office_cc', 'In Office Credit Card'),
+        ('offline_cc', 'In Office Credit Card'),
         ('cash', 'Cash'),
     )
-    type = models.CharField(choices=TYPE_CHOICES)
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateTimeField(auto_now_add=True)
     email = models.EmailField()
