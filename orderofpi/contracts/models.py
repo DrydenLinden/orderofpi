fb488738e71a4df2cec40cb3744f9760df4d47d7
cthib <cortland.thibodeau@gmail.com>
2017-07-10 11:16:38 -0700

    First stab at the database

--- orderofpi/contracts/models.py
+++ orderofpi/contracts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+# Contract model. Used for...
+class Contract(models.Model):
+    target = models.CharField(max_length=30)
+    issuer = models.CharField(max_length=30)
+    indicated_value = models.DecimalField(max_digits=10, decimal_places=2)
+    issued_date = models.DateField(auto_now_add=True)
+
+    # If the contract allows funds to be added using an extension id
+    extend_id = models.CharField(max_length=60, null=True)
+
+    # OoP Trial information
+    trial_date = models.DateTimeField()
+    trial_location = models.CharField(max_length=30)
+
+
+# Contract notes model. Extension of the contract
+class ContractNotes(models.Model):
+    contract = models.ForeignKey(Contract, on_delete=models.CASCADE)
+    author = models.CharField(max_length=30)
+    text = models.CharField(max_length=200)
--- orderofpi/payments/models.py
+++ orderofpi/payments/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from ..contracts.models import Contract
 
-# Create your models here.
+
+# Transaction model
+class Transaction(models.Model):
+    contract = models.ForeignKey(Contract, on_delete=models.CASCADE)
+
+    # Transaction basic details
+    TYPE_CHOICES = (
+        ('online_cc', 'Online Credit Card'),
+        ('office_cc', 'In Office Credit Card'),
+        ('cash', 'Cash'),
+    )
+    type = models.CharField(choices=TYPE_CHOICES)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField()
+
+    # Stripe attributes, can be empty due to cash transaction
+    stripe_status = models.CharField(max_length=50, null=True)
+    stripe_id = models.CharField(max_length=20, null=True)
