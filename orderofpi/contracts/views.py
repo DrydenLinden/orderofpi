57dc9e2be698e3eb70d4c60e149160ad07a7e861
justinmacaulay <jmac@uvic.ca>
2017-07-24 12:36:10 -0700

    added a UTF 8 encoding comment to have the kanji display properly.

--- orderofpi/contracts/views.py
+++ orderofpi/contracts/views.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, reverse, redirect, get_object_or_404
