52d4dff9d3807f5a280c120de167b1235aa0ab58
justinmacaulay <jmac@uvic.ca>
2017-07-24 19:38:50 -0700

    added HTML file for print contract and admin action

--- orderofpi/contracts/admin.py
+++ orderofpi/contracts/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
+from django.shortcuts import render
 
 from .models import Contract, ContractNotes
+
+def print_contract(modeladmin, request, queryset):
+
+    template = "contracts/print_contract.html"
+
+    if queryset.count() != 1:
+        modeladmin.message_user(request, "Can not print more than one contract at a time.")
+        return
+
+    for item in queryset:
+        contract = item
+
+    context = {
+        'contract_info': contract,
+    }
+
+    return render(request, template, context)
+
+print_contract.short_description = "Print contract for the trial."
 
 class ContractNotesInline(admin.TabularInline):
     model = ContractNotes
@@ -10,6 +30,7 @@ class ContractNotesInline(admin.TabularInline):
 
 class ContractAdmin(admin.ModelAdmin):
     inlines = [ContractNotesInline]
+    actions = [print_contract]
 
 
 admin.site.register(Contract, ContractAdmin)
